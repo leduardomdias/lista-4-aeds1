e8b6bf14e24b327082b70e479392a01dec7374df
Luís Eduardo Marques Dias <leduardomdias@gmail.com>
2024-10-08 17:00:55 -0300

    att

--- ex14.c
+++ ex14.c
@@ -1,26 +1,55 @@
 #include <stdio.h>
 
 float func(int n){
-  float media = 0;
-  float nota = 0;
-  float soma = 0;
+    float nota = 0;
+    float soma_aprovados = 0;
+    int cont_aprovados = 0;
 
-  for(int i=1;i<=n;i++)
-  {
-    printf("Digite a nota do aluno %d:\n", i);
-    scanf("%f", &nota);
-    soma += nota;
-  }
-  media = soma / n;
-  return media;
+    for(int i = 1; i <= n; i++)
+    {
+        do {
+            printf("Digite a nota do aluno %d (entre 0 e 10):\n", i);
+            scanf("%f", &nota);
+
+            if (nota < 0 || nota > 10)
+            {
+                printf("Nota inválida! Por favor, insira um número entre 0 e 10.\n");
+            }
+        } while (nota < 0 || nota > 10);
+
+        if (nota >= 6) {
+            printf("Aluno %d aprovado!\n", i);
+            soma_aprovados += nota;
+            cont_aprovados++;
+        } else {
+            printf("Aluno %d reprovado!\n", i);
+        }
+    }
+
+    if (cont_aprovados > 0) {
+        return soma_aprovados / cont_aprovados;
+    } else {
+        return 0;
+    }
 }
 
 int main(){
-  int n;
+    int n;
 
-  printf("Digite um numero de alunos:\n ");
-  scanf("%d", &n);
-  float media = func(n);
-  printf("Media = %.2f", media);
-  return 0;
+    printf("Digite o número de alunos:\n");
+    scanf("%d", &n);
+
+    if (n > 0) {
+        float media = func(n);
+
+        if (media > 0) {
+            printf("Média dos alunos aprovados = %.2f\n", media);
+        } else {
+            printf("Nenhum aluno foi aprovado.\n");
+        }
+    } else {
+        printf("O número de alunos deve ser maior que zero.\n");
+    }
+
+    return 0;
 }
